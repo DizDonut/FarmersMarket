30ffb6f4c343ea8b61aea18b36d02ae9c58584ef
Michael R. Bechtel <mikebec20@gmail.com>
2017-07-18 21:50:18 -0400

    dynami maps v1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
 
             //call renderResults function, passing the map variable as an argument
             renderResults(map);
-            getResults(zipCode);
+            getFirstResults(zipCode);
 
         }); //end submit on click event
 
@@ -53,15 +53,24 @@ $(document).ready(function() {
           url variable, google maps embed API key, and other pertinent q (query) information
           @param zip: zip will be the value returned from the user input form
         */
-        function initMap(zip) {
+        function initMap(address) {
             url += "?" + $.param({
                 "key": "AIzaSyC-esHVNQ4muZerDSPt3ChxUd8-agTMc_c",
-                "q": "Farmers+Market+near" + zip
+                "q": address
             });
             return url;
         };
 
-        function getResults(zip) {
+        /*
+          getFirstResults function accepts one parameter and makes an ajax call using the USDA API to pull back
+          the ID of each farmers market within a certain radius of the given zip code.  This function will also dynamically
+          create collapsible elements on the page and place divs within those elements, that will later be populated
+          on the call of the getSecondResults function.  Returns the ID of the farmers markets
+
+          @param zip: the value of the zip code entered by the user on the search page
+        */
+
+        function getFirstResults(zip) {
             var id = "";
             var name = "";
 
@@ -96,13 +105,58 @@ $(document).ready(function() {
                     // append each returned result as a list item to the DOM
                     popoutList.append(listItem + popoutHeader + popoutBody);
                     $("#ajaxResults").append(popoutList);
-                }
+                } //end for loop for dynamic collapse elements
 
                 scrapeFoodsInSeason();
 
             }); //end ajax call
 
+            getSecondResults(id);
+
         } //end getResults function
+
+
+        /*
+          getSecondResults takes one parameter, makes a second call to the USDA api
+          and pulls back the marketdetails associated with the ID that we pass into
+          the parameter.  Using the marketdetails, this function will also dynamically
+          create iframe elements where a call to the google maps embed API will show the
+          location on a google map element
+
+          @param argID: value returned from the getFirstResults function
+        */
+
+        function getSecondResults(argID){
+        $.ajax({
+          ////////////////////////
+          /*  ajax call to be
+              to be completed
+              by Steve         */
+          ///////////////////////
+
+
+        }).done(function(response){
+          var details = response.marketdetails;
+          console.log(details);
+
+            for (var i = 0; i < details.length; i++) {
+              var mapID = details[i].GoogleLink;
+              var address = details[i].Address;
+
+              var source = initMap(address);
+              console.log(source);
+
+              var iframes = $("<iframe>", {
+                src: source,
+                frameborder: "0",
+                zoom: "10",
+                width: "100%",
+                height: "100%"
+              }).appendTo(".collapsible-body");
+            }; //end for loop
+          }); //end ajax call
+
+        }; //end getSecondResults function
 
         // allows for hamburger menu collapse to work
         $(".button-collapse").sideNav();
