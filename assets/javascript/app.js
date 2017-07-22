3b413f31bbbb020846f4e2ba43281ca9eb94eb38
Jim Gray <JimGray9999@users.noreply.github.com>
2017-07-21 21:29:42 -0400

    front end updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
         $('.scrollspy').scrollSpy();
         $(".button-collapse").sideNav();
 
-
         $(document).on("click", "#submit", function(event) {
             event.preventDefault();
 
@@ -104,8 +103,6 @@ var ourFunctions = {
         $("#ajaxResults").append(popoutList);
       } //end for loop for dynamic collapse elements
 
-      ourFunctions.scrapeFoodsInSeason();
-
     }); //end ajax call
   },
 
@@ -136,52 +133,55 @@ var ourFunctions = {
     }); //end ajax call
   },
 
-//on load: display foods in season based on current month in the DOM
-        foodsInSeason: function () {
-            // (moment.js for current month)
-            var currentMonth = moment().month();
-            var currentMonthText = moment().format('MMMM');
-            console.log("Current Month: " + currentMonth);
-            $("#current-month").text("Foods in season for the month of " + currentMonthText);
+  //on load: display foods in season based on current month in the DOM
+  foodsInSeason: function () {
+      // (moment.js for current month)
+      var currentMonth = moment().month();
+      var currentMonthText = moment().format('MMMM');
+      console.log("Current Month: " + currentMonth);
+      $("#current-month").text("Foods in season for the month of " + currentMonthText);
 
-            // JSON data obtained via web crawler Apifier API:
-            // https://www.apifier.com/crawlers/DpP4r2ouwftwZT5Ym
-            var foodsDataURL = "https://api.apifier.com/v1/execs/vm5CwJ6Rr6ePdugwK/results";
+      // JSON data obtained via web crawler Apifier API:
+      // https://www.apifier.com/crawlers/DpP4r2ouwftwZT5Ym
+      var foodsDataURL = "https://api.apifier.com/v1/execs/vm5CwJ6Rr6ePdugwK/results";
 
-            $.ajax({
-              type: "GET",
-              contentType: "application/json",
-              url: foodsDataURL
-            })
-            .done(function(response){
-              var foodString = response[currentMonth].pageFunctionResult.foods;
-              
-              // remove all /n from foodString, then remove blank items
-              foodString = foodString.replace(/(\r\n|\n|\r)/gm,',').trim();
-              var foodArray = foodString.split(',');
+      $.ajax({
+        type: "GET",
+        contentType: "application/json",
+        url: foodsDataURL
+      })
+      .done(function(response){
+        var foodString = response[currentMonth].pageFunctionResult.foods;
+        
+        // remove all /n from foodString array
+        foodString = foodString.replace(/(\r\n|\n|\r)/gm,',').trim();
+        var foodArray = foodString.split(',');
 
-              // remove blank items in array
-              for(var i = foodArray.length-1; i >= 0; i--){  
-                  if(foodArray[i] === ''){           
-                      foodArray.splice(i,1);               
-                  }
-              }
-              console.log(foodArray);
+        // remove blank items in foodString array
+        for(var i = foodArray.length-1; i >= 0; i--){  
+            if(foodArray[i] === ''){           
+                foodArray.splice(i,1);               
+            }
+        }
+        console.log(foodArray);
 
-              $("#foodTable > tbody").append("<tr><td>" + foodArray[0] + "</td></tr>");
+        $("#foodTable > tbody").append("<tr><td>" + foodArray[0] + "</td></tr>");
 
-              for (i = -1 ; i < (foodArray.length - 3) ; i+=2){
-                $("#foodTable > tbody").append("<tr><td>" + foodArray[i + 2] + "</td><td>"
-                + foodArray[i+3] + "</td></tr>");
-              }
-            });
-        }, // end foodsInSeason() function
+        for (i = -1 ; i < (foodArray.length - 3) ; i+=2){
+          $("#foodTable > tbody").append("<tr><td>" + foodArray[i + 2] + "</td><td>"
+          + foodArray[i+3] + "</td></tr>");
+        }
+      });
+  }, // end foodsInSeason() function
 
     createComments: function(){
       var commentModal = ("<button class='waves-effect waves-light btn modal-trigger' data-target='modal1'>Leave a Comment!</button>")
-      $(".collapsible-body").append(commentModal);
+
+      $(".collapsible-body").html(commentModal);
     }
 
   }//end function object
+
+  ourFunctions.foodsInSeason(); // show foods in season on document load
 
 }) //end document ready
